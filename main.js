f525251c4eb49aa4410a8eb180877f146292eec7
eakkaphop pengtham <ttobas@hotmail.com>
2020-11-02 17:38:55 +0700

    test callback("faill")

--- main.js
+++ main.js
@@ -1,7 +1,28 @@
-const hello = require('./app');
+//เรียก module จากอีกไฟล์
+//const hello = require('./app');
 
 
-hello.hello();
-console.log(hello.name);
+
+//hello.hello();
+//console.log(hello.name);
 
 //testing git
+
+
+/*const Hello = name => console.log(`Hello $(name)`);
+const mycallback = callback => callback('Eakkphop');
+
+mycallback(Hello);*/
+const hello = (error , name ) => {
+    if(error){
+        throw error
+    }
+    console.log(`Hello $name`);
+}
+
+const mycallback = callback => {
+    if(!callback){
+        callback(new Error("No calllback"))
+    }
+    callback(null,"Eakkaphop");
+}
